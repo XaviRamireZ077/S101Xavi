479df509843370a26a0481d842a6038b27c099fe
XaviRamireZ077 <ramirezinfantesxavi@gmail.com>
2022-07-04 18:40:49 +0200

    entrega proyecto

--- src/n1exercici2/Cotxe.java
+++ src/n1exercici2/Cotxe.java
@@ -1,59 +1,41 @@
 package n1exercici2;
 
 public class Cotxe {
-	
-	private static final String marca; 
-	private static String model;
+
+	private static final String marca = "Ferrari";
+	private static String model = "Testarrosa";
 	private final double potencia;
-	
-	
-	
-	
-	//Constructor
-	
-	public Cotxe(String marca, String model, double potencia) {
+
+	// Constructor
+
+	public Cotxe(double potencia) {
 		super();
-		this.model = model;
+
 		this.potencia = potencia;
 	}
-
-
-
 
 	public static String getModel() {
 		return model;
 	}
 
-
-
-
 	public static void setModel(String model) {
 		Cotxe.model = model;
 	}
 
-
-
+	public static String getMarca() {
+		return marca;
+	}
 
 	public double getPotencia() {
 		return potencia;
 	}
-	
-	
-	
-	static void frenar() {
-		
-		
+
+	public static void frenar() {
+		System.out.println("El cotxe esta frenant");
 	}
-	
-	
+
 	public void accelerar() {
-		
-		
-		
-		
-		
-		
+		System.out.println("El cotxe esta accelerant");
 	}
-	
 
 }
--- src/n1exercici2/Maincotxe.java
+++ src/n1exercici2/Maincotxe.java
@@ -17,26 +17,26 @@ public class Maincotxe {
 	 * classe principal.
 	 */
 
+	public static void main(String []args) {
+		
+		
+		Cotxe cotxe1 = new Cotxe(500);
+		
+		cotxe1.accelerar();
+		
+		Cotxe.frenar();
+		
+		
+		
+	}
 	
 	
-	Cotxe vehicle1 = new Cotxe("Ferrari","testarrosa",500);
-    Cotxe vehicle2 = new Cotxe("Lamborgini","diablo",480);
+	
+	
+    
 	
 	
    
-    vehicle1.accelerar();
-    System.out.println(vehicle1.getVelocidad());
-    vehicle1.parar();
-    System.out.println(vehicle1.getVelocidad());
-    System.out.println(vehicle1.toString());
-    vehicle2.accelerar();
-    System.out.println(vehicle2.toString());
-    vehicle3.accelerar();
-    System.out.println(vehicle3.toString());
-    vehicle4.accelerar();
-    System.out.println(vehicle4.toString());
-}
-	
 	
 	
 	
